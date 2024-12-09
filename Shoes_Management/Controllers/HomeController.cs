0cda748622a02099434a278768168d3831f2b0c6
trungtuan200304 <tran.trung.tuan.200304@gmail.com>
2024-12-09 22:08:03 +0700

    thêm trang register cho khách hàng chưa đăng nhập

--- Shoes_Management/Controllers/HomeController.cs
+++ Shoes_Management/Controllers/HomeController.cs
@@ -22,6 +22,11 @@ namespace Shoes_Management.Controllers
 			return View();
 		}
 
+		public IActionResult Register()
+		{
+			return View();
+		}
+
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
 		{
