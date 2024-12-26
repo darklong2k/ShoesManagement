981abceccd5d8bac56583d6ed4a0e5c7a6edab80
PhucNguyen <admin@LAPTOP-GFK5C0EF>
2024-12-26 20:11:26 +0700

    NBTRungPhuc_Layout,fix trang chu

--- Shoes_Management/Controllers/HomeAPIController.cs
+++ Shoes_Management/Controllers/HomeAPIController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Shoes_Management.Models;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Shoes_Management.Controllers
 {
@@ -14,10 +17,13 @@ namespace Shoes_Management.Controllers
             _context = context;
         }
 
+        [HttpGet("LayOut")]
         public IActionResult LayOut()
         {
-            var cate = _context.Categories;
-            return Ok(cate);
+            var cate = _context.Categories.Take(2);
+            var brand = _context.Brands;
+            var website = _context.Websites.First();
+            return Ok(new {cate,brand,website });
         }
 
         [HttpGet("GetProducts")]
@@ -29,6 +35,8 @@ namespace Shoes_Management.Controllers
             var bestSeller = (from pd in _context.ProductDetails
                               join od in _context.OrderDetails on pd.ProductDetailId equals od.ProductDetailId
                               join p in _context.Products on pd.ProductId equals p.ProductId
+                              join o in _context.Orders on od.OrderId equals o.OrderId
+                              where o.Status == "Delivered"
                               group od by p into grouped
                               select new
                               {
