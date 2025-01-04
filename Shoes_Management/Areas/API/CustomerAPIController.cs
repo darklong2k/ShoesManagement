5e71617ee7f74a6ab951c47fa10d9bfdf0716ec5
trungtuan200304 <tran.trung.tuan.200304@gmail.com>
2025-01-04 14:05:51 +0700

    thêm api, view, controller customer

---
+++ Shoes_Management/Areas/API/CustomerAPIController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shoes_Management.Models;
+
+namespace Shoes_Management.Areas.API
+{
+    [Route("api/customers")]
+    [ApiController]
+    public class CustomerAPIController : ControllerBase
+    {
+        private readonly Shoescontext _shoescontext;
+        public CustomerAPIController(Shoescontext shoescontext)
+        {
+            _shoescontext = shoescontext;
+        }
+        [HttpGet]
+        public IActionResult GetCustomer()
+        {
+            var customers = _shoescontext.Customers.ToList();
+            if (customers == null)
+            {
+                return BadRequest(new {message = "Don't have any customers"});
+            }
+            return Ok(_shoescontext.Customers.ToList());
+        }
+    }
+}
