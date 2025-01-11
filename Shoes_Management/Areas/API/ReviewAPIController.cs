78cccc11f5438e0b0d5a15e6a409dec1a6a4762f
trungtuan200304 <tran.trung.tuan.200304@gmail.com>
2025-01-11 08:34:01 +0700

    thêm chức năng review

---
+++ Shoes_Management/Areas/API/ReviewAPIController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shoes_Management.Models;
+
+namespace Shoes_Management.Areas.API
+{
+    
+    [Route("api/reviews")]
+    [ApiController]
+    public class ReviewAPIController : ControllerBase
+    {
+        private readonly Shoescontext _shoescontext;
+        public ReviewAPIController(Shoescontext shoescontext)
+        {
+            _shoescontext = shoescontext;
+        }
+        [HttpGet]
+        public IActionResult GetReview()
+        {
+            var query = from c in _shoescontext.Customers
+                        join r in _shoescontext.Reviews on c.CustomerId equals r.CustomerId
+                        join pd in _shoescontext.ProductDetails on r.ProductDetailId equals pd.ProductDetailId
+                        join p in _shoescontext.Products on pd.ProductId equals p.ProductId
+                        join co in _shoescontext.Colors on pd.ColorId equals co.ColorId
+                        join s in _shoescontext.Sizes on pd.SizeId equals s.SizeId
+                        select new
+                        {
+                            CustomerId = c.CustomerId,
+                            CustomerName = c.Name,
+                            Comment = r.Comment,
+                            Rating = r.Rating,
+                            ReviewStatus = r.Status,
+                            ProductName = p.Name,
+                            ColorName = co.ColorName,
+                            SizeName = s.SizeName,
+                            ProductDetailId = pd.ProductDetailId
+                        };
+
+            var reviews = query.ToList();
+
+
+
+            if (reviews == null) return BadRequest();
+
+            return Ok(reviews);
+        }
+        public Review GetReviewByProductDetailIdAndCustomerId(int product_detail_id, int customer_id)
+        {
+            var reviews = _shoescontext.Reviews.ToList();
+            var review = reviews.FirstOrDefault(review => review.CustomerId == customer_id && review.ProductDetailId == product_detail_id);
+            return review;
+        }
+        [HttpPatch("{product_detail_id}/{customer_id}/{status}")]
+        public IActionResult ChangeStatusReview(int product_detail_id, int customer_id, string status) 
+        {
+            var review = GetReviewByProductDetailIdAndCustomerId(product_detail_id, customer_id);
+            
+            if (review == null) return BadRequest();
+
+            review.Status = status;
+            try
+            {
+                // Save changes to the database
+                _shoescontext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                // Handle any errors that occur during saving
+                return StatusCode(500, "Internal server error: " + ex.Message);
+            }
+            return Ok(review);
+        }
+    }
+}
